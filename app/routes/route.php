2608c0bbe9f51b9ffa51725eafe493e27e380341
rksam <erksg.10.26@gmail.com>
2025-02-24 14:01:10 -0500

    update route

--- app/routes/route.php
+++ app/routes/route.php
@@ -27,18 +27,17 @@ $db = new Database();
 
 // Instancia del controlador con la base de datos
 
-$userController = new UserController(new Database());
-$miembroController = new MiembroController(new Database());
-$logroController = new LogroController(new Database());
-$contactoController = new ContactoController(new Database());
-$personaController = new PersonaController(new Database());
-$eventoPersonaController = new EventoPersonaController(new Database());
-$eventoController = new EventoController(new Database());
-$eventoMiembroController = new EventoMiembroController(new Database());
-$noticiaController = new NoticiaController(new Database());
-$noticiaPersonaController = new NoticiaPersonaController(new Database());
-$miembrosLogrosController = new MiembrosLogrosController(new Database());
-// Rutas de autenticación
+$userController = new UserController($db);
+$miembroController = new MiembroController($db);
+$logroController = new LogroController($db);
+$contactoController = new ContactoController($db);
+$personaController = new PersonaController($db);
+$eventoPersonaController = new EventoPersonaController($db);
+$eventoController = new EventoController($db);
+$eventoMiembroController = new EventoMiembroController($db);
+$noticiaController = new NoticiaController($db);
+$noticiaPersonaController = new NoticiaPersonaController($db);
+$miembrosLogrosController = new MiembrosLogrosController($db);
 $userController = new UserController($db);
 
 // -----------------------
