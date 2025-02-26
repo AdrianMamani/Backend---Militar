aca54b09307683e0a01b6f417667bdf406c4bb41
AnthonyJCA16 <72991557+DSWAnthony@users.noreply.github.com>
2025-02-26 15:53:10 -0500

    Added endpoint protection

--- app/model/Logro.php
+++ app/model/Logro.php
@@ -32,8 +32,8 @@ class Logro
 
     public function getData()
     {
-        $query = "SELECT * FROM logro;";
-        $stm = $this->db->query($query);
+        $query = "SELECT * FROM Logro;";
+        $stm = $this->db->query($query);    
 
         if (!$stm) {
             error_log("Ocurrio un error al preparar la consulta " . $this->db->error);
@@ -41,12 +41,6 @@ class Logro
         }
 
         $listMiembro = $stm->fetch_all(MYSQLI_ASSOC);
-
-        if (empty($listMiembro)) {
-            error_log("No se encontraron registros en la tabla miembro " . $this->db->error);
-            return null;
-        }
-
         $stm->free();
         return $listMiembro;
     }
@@ -54,7 +48,7 @@ class Logro
     public function postData($titulo, $descripcion)
     {
 
-        $query = "INSERT INTO logro (titulo,descripcion) VALUES (?,?)";
+        $query = "INSERT INTO Logro (titulo,descripcion) VALUES (?,?)";
         $stm = $this->db->prepare($query);
         if (!$stm) {
             error_log("hubo un error al preparar la consulta" . $this->db->error);
@@ -75,7 +69,7 @@ class Logro
 
     public function putData($id, $titulo, $descripcion)
     {
-        $query = "UPDATE logro SET  titulo=? , descripcion=? WHERE id_logro = ?";
+        $query = "UPDATE Logro SET  titulo=? , descripcion=? WHERE id_logro = ?";
         $stm = $this->db->prepare($query);
         if (!$stm) {
             error_log("Ocurrio un problema al preparar la query" . $this->db->error);
@@ -92,7 +86,7 @@ class Logro
 
     public function deleteData($id)
     {
-        $query = "DELETE FROM logro WHERE  id_logro = ?";
+        $query = "DELETE FROM Logro WHERE  id_logro = ?";
         $stm =  $this->db->prepare($query);
         if (!$stm) {
             error_log("Ocurrio un error al prepara la consulta" . $this->db->error);
